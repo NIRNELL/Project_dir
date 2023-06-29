fea0dad26d1882ebbe772129d3e0fa85d487fc76
NiRNELL <step_lerr@mail.ru>
2023-06-29 14:04:20 +0500

    First commit

---
+++ Projang/Projang/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Projang project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projang.settings')
+
+application = get_wsgi_application()
